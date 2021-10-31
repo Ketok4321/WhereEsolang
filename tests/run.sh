c96dae9805fc2cdaa37cb1e0b3dfff4aa708b176
“ketoK” <ketok4321@gmail.com>
2021-10-31 15:33:47 +0100

    Added tests

---
+++ tests/run.sh
@@ -0,0 +1,17 @@
+PROJECT_PATH="../src/WhereEsolang.Cli"
+BUILD_PATH="$PROJECT_PATH/bin/Debug/net5.0/WhereEsolang.Cli"
+
+dotnet build $PROJECT_PATH
+
+test(){
+    res=$($BUILD_PATH $1)
+    if [[ $res = $2 ]]
+    then
+        echo -e "\e[92m'$1' passed"
+    else
+        echo -e "\e[91m'$1' failed (expected $2, got $res)"
+    fi
+}
+
+test "./output.wh" "1"
+test "./while.wh" "10"
