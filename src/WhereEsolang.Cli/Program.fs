c59d900bbead1fe3b27d8b9e163ffd2745df151c
Ketok <ketok4321@gmail.com>
2022-01-16 19:34:02 +0100

    Where Interactive.

--- src/WhereEsolang.Cli/Program.fs
+++ src/WhereEsolang.Cli/Program.fs
@@ -4,10 +4,27 @@ open WhereEsolang.Parser
 open WhereEsolang.Interpreter
 open FParsec
 
-let path = Environment.GetCommandLineArgs().[1]
-let code = File.ReadAllText(path)
+let args = Environment.GetCommandLineArgs()
 
-match run Parser.program code with
-| Success (res, _, _) ->
-    Interpreter.run res (Interpreter.createMemoryCells 4uy)
-| Failure (message, error, _) -> Console.WriteLine(message)
+if args.Length = 1 then   
+    let cells = Interpreter.createMemoryCells 4uy
+    
+    while true do
+        for cell in cells do
+                Console.Write($"[{cell.contents}]")
+        Console.WriteLine()
+        
+        Console.Write("> ")
+        let command = Console.ReadLine()
+        
+        match run (Parser.Statement.any .>> Parser.whitespace) command with
+        | Success (res, _, _) ->
+            Interpreter.run [res] cells
+        | Failure (message, error, _) -> Console.WriteLine(message)
+else
+    let code = File.ReadAllText(args.[1])
+
+    match run Parser.program code with
+    | Success (res, _, _) ->
+        Interpreter.run res (Interpreter.createMemoryCells 4uy)
+    | Failure (message, error, _) -> Console.WriteLine(message)
